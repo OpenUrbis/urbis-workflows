6279df8e7f8cbd403827fe9fe1c898a176b00f3d
RenanTashiro <renantashiro@hotmail.com>
2026-03-06 11:40:00 -0300

    temp: confirm sign up page need to contact support only

--- apps/web/src/modules/auth/ConfirmForgetPassword.tsx
+++ apps/web/src/modules/auth/ConfirmForgetPassword.tsx
@@ -96,7 +96,6 @@ export function ConfirmForgetPassword(): JSX.Element {
           <FormControl id="code">
             <FormLabel>Código de Confirmação *</FormLabel>
             <Input
-              autoFocus
               placeholder="Código de confirmação"
               type={"text"}
               size="lg"
@@ -130,8 +129,7 @@ export function ConfirmForgetPassword(): JSX.Element {
           </p>
           {error && <p className="text-red-500">{error}</p>}
           <p className="text-sm font-bold">
-            Foi enviado um código de confirmação no e-mail cadastrado. Caso não
-            recebeu aguarde um momento e verifique a caixa de Spam.
+            Comunique o suporte via e-mail e peça o código de confirmação
           </p>
           <button
             type="submit"
